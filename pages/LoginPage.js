a928a8dc48d7f90c594478ea97f3648bfd675476
nitishjha18 <nitishjha1100@gmail.com>
2026-07-28 12:13:29 +0530

    Implemented POM for login and created a test case for comparing cart and remove button

---
+++ pages/LoginPage.js
@@ -0,0 +1,16 @@
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.username = page.locator("#user-name");
+    this.password = page.locator("#password");
+    this.loginButton = page.locator("#login-button");
+  }
+
+   async login(username, password) {
+    await this.username.fill(username);
+    await this.password.fill(password);
+    await this.loginButton.click();
+  }
+}
+
+module.exports = {LoginPage};
--- tests/bttnStyling.spec.js
+++ tests/bttnStyling.spec.js
@@ -1,4 +1,5 @@
 const {test, expect} = require("@playwright/test");
+const {LoginPage} = require("../pages/LoginPage");
 
 test.describe("Testing the button stylling scenerios", () => {
     
@@ -13,5 +14,38 @@ test.describe("Testing the button stylling scenerios", () => {
         expect(loginButton).toBe("rgb(61, 220, 145)");
         console.log(`The expected color of login button is ${loginButton}`);
     });
+
+    test("Verifying cart button color after clicking with remove", async({page}) => {
+        await page.goto("https://www.saucedemo.com/");
+        await page.waitForLoadState("networkidle");
+
+        const loginPage = new LoginPage(page);
+        await loginPage.login("standard_user", "secret_sauce");
+
+        // await page.locator("#user-name").fill("standard_user");
+        // await page.locator("#password").fill("secret_sauce");
+        // await page.locator("#login-button").click();
+
+        const cartBttn = await page.locator("#add-to-cart-sauce-labs-backpack").evaluate((element) => {
+            return window.getComputedStyle(element).color;
+        }); 
+
+        expect(cartBttn).toBe("rgb(19, 35, 34)");
+        console.log(`The expected color of cart button is ${cartBttn}`);
+
+
+        await page.locator("#add-to-cart-sauce-labs-backpack").click();
+        
+        const removeBttn = await page.locator("#remove-sauce-labs-backpack").evaluate((element) => {
+            return window.getComputedStyle(element).color;
+        });
+        expect(removeBttn).toBe("rgb(226, 35, 26)");
+        console.log(`The expected color of remove button is ${removeBttn}`);
+
+
+
+    });
+
+
 
 });
